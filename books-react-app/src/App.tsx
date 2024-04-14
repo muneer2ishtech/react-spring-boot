bf2c35bf5c2f77f0a5facf5a609742fa53f07605
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-04-14 16:47:57 +0300

    fix App.tsx

--- books-react-app/src/App.tsx
+++ books-react-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import BooksList from './components/books/BooksList';
 import NewBook from './components/books/NewBook';
@@ -9,24 +9,21 @@ import SignUp from './components/auth/SignUp';
 import SignIn from './components/auth/SignIn';
 import SignOut from './components/auth/SignOut';
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" component={BooksList} />
+const App: React.FC = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Navigate to="/books" />} />
 
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/signout" component={SignOut} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signout" component={SignOut} />
 
-          <Route exact path="/books/new" component={NewBook} />
-          <Route exact path="/books/:id" component={ViewBook} />
-          <Route exact path="/books/:id/edit" component={EditBook} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+      <Route exact path="/books" component={BooksList} />
+      <Route exact path="/books/new" component={NewBook} />
+      <Route exact path="/books/:id" component={ViewBook} />
+      <Route exact path="/books/:id/edit" component={EditBook} />
+    </Routes>
+  </Router>
+);
 
 export default App;
