ef463110c1fc0be684c52318874d9331756d6b7c
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-04-14 22:18:09 +0300

    handle unique constraint with bad request

--- books-springboot-app/src/main/java/fi/ishtech/practice/books/controller/CustomExceptionHandler.java
+++ books-springboot-app/src/main/java/fi/ishtech/practice/books/controller/CustomExceptionHandler.java
@@ -2,6 +2,8 @@ package fi.ishtech.practice.books.controller;
 
 import java.util.NoSuchElementException;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
@@ -33,6 +35,18 @@ public class CustomExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException ex) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 				.body(ErrorResponse.create(ex, HttpStatus.NOT_FOUND, ex.getMessage()));
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+		log.error("DB Constraint failed: {}", ex.getMessage());
+		if (StringUtils.containsIgnoreCase(ex.getMessage(), "unique")) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(ErrorResponse.create(ex, HttpStatus.BAD_REQUEST, ex.getMessage()));
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(ErrorResponse.create(ex, HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()));
+		}
 	}
 
 }
