6085480eb2ee74a755fecd6d0b2647dfdc40f18b
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-04-15 07:41:48 +0300

    interface for page

--- books-react-app/src/interfaces.ts
+++ books-react-app/src/interfaces.ts
@@ -24,3 +24,33 @@ export interface Book {
     year: number;
     price: number;
 }
+
+interface Page<T> {
+    content: T[];
+    pageable: {
+        sort: {
+            sorted: boolean;
+            unsorted: boolean;
+            empty: boolean;
+        };
+        pageNumber: number;
+        pageSize: number;
+        offset: number;
+        unpaged: boolean;
+        paged: boolean;
+    };
+    totalPages: number;
+    totalElements: number;
+    last: boolean;
+    size: number;
+    number: number;
+    sort: {
+        sorted: boolean;
+        unsorted: boolean;
+        empty: boolean;
+    };
+    numberOfElements: number;
+    first: boolean;
+    empty: boolean;
+    // Add other properties as needed
+}
