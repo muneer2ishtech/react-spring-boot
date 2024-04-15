82b686ae663ad4b95532afb46b432353b74d79a6
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-04-15 07:38:08 +0300

    new book interface to handle null id

--- my-react-app/src/interfaces.ts
+++ my-react-app/src/interfaces.ts
@@ -1,3 +1,11 @@
+export interface NewBook {
+    id: null;
+    title: string;
+    author: string;
+    year: number;
+    price: number;
+}
+
 export interface Book {
     id: number;
     title: string;
