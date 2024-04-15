ff90879f93dd94570f02d44ae30fe06d88b23052
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-04-15 06:40:53 +0300

    fix app tsx

--- books-react-app/src/App.tsx
+++ books-react-app/src/App.tsx
@@ -6,24 +6,19 @@ import BooksList from './components/books/BooksList';
 import NewBook from './components/books/NewBook';
 import ViewBook from './components/books/ViewBook';
 import EditBook from './components/books/EditBook';
-import SignUp from './components/auth/SignUp';
-import SignIn from './components/auth/SignIn';
-import SignOut from './components/auth/SignOut';
 
 const App: React.FC = () => (
   <Router>
     <SideNav />
     <Routes>
-      <Route exact path="/" element={<Navigate to="/books" />} />
+      <Route path="/" element={<Navigate to="/books" />} />
 
-      <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/signin" component={SignIn} />
-      <Route exact path="/signout" component={SignOut} />
+      <Route path="/books" element={<BooksList />} />
+      <Route path="/books/new" element={<NewBook />} />
+      <Route path="/books/:id" element={<ViewBook />} />
+      <Route path="/books/:id/edit" element={<EditBook />} />
 
-      <Route exact path="/books" component={BooksList} />
-      <Route exact path="/books/new" component={NewBook} />
-      <Route exact path="/books/:id" component={ViewBook} />
-      <Route exact path="/books/:id/edit" component={EditBook} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </Router>
 );
