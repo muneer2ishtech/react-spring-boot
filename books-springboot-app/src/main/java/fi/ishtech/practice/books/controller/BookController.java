efd34a277cc93e2ce5fde3ca4ac7447bc763f775
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-04-17 20:34:56 +0300

    crossorigin - all methods

--- books-springboot-app/src/main/java/fi/ishtech/practice/books/controller/BookController.java
+++ books-springboot-app/src/main/java/fi/ishtech/practice/books/controller/BookController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -37,7 +38,8 @@ import lombok.extern.slf4j.Slf4j;
  * @author Muneer Ahmed Syed
  */
 @RestController
-@CrossOrigin
+@CrossOrigin(methods = { RequestMethod.GET, RequestMethod.HEAD, RequestMethod.POST, RequestMethod.PUT,
+		RequestMethod.PATCH, RequestMethod.DELETE, RequestMethod.OPTIONS, RequestMethod.TRACE })
 @Slf4j
 public class BookController {
 
